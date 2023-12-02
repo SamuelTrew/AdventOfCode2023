fdbcb332fe385c2b3d212dd31705455694015a04
Samuel Trew <samuel.trew@outlook.com>
2023-12-02 01:01:55 +0000

    Did part1 day 1

--- days/1.rkt
+++ days/1.rkt
@@ -1,6 +1,18 @@
 #lang racket
 
-(define day1 "Day 1")
+(define inputs
+   (map (lambda (line)
+      (filter char-numeric? (string->list line)))
+   (file->lines "inputs/1_1.txt"))
+)
+
+(define combinations
+   (map (lambda (lst)
+      (string->number (apply string (list (first lst) (last lst))))
+   ) inputs)
+)
+
+(define day1 (foldr + 0 combinations))
 
 
 (provide day1)
