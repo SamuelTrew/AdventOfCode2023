cd294610cb752b3a6a289f48e4d9ab8461327f31
Samuel Trew <samuel.trew@outlook.com>
2023-12-06 02:36:36 +0000

    Racket errors suck - day 1 done

--- days/1.rkt
+++ days/1.rkt
@@ -1,18 +1,65 @@
-#lang racket
+#lang typed/racket
+; (require srfi/13)
+(require/typed srfi/13 [string->list (-> String (Listof Char))])
 
-(define inputs
-   (map (lambda (line)
-      (filter char-numeric? (string->list line)))
-   (file->lines "inputs/1_1.txt"))
+(define lines : (Listof String)
+   (file->lines "inputs/1.txt")
 )
 
-(define combinations
-   (map (lambda (lst)
-      (string->number (apply string (list (first lst) (last lst))))
-   ) inputs)
+(define (input-filter [lines : (Listof String)]) : (Listof (Listof Char))
+   (map
+      (lambda ([line : String]) : (Listof Char)
+         (filter char-numeric? (string->list line))
+      )
+      lines
+   )
 )
 
-(define day1 (foldr + 0 combinations))
+(define (combine-filtered [inputs : (Listof String)]) : (Listof Integer)
+   (map
+      (lambda ([lst : (Listof Char)]) : Integer
+         (cast (string->number (list->string (list (first lst) (last lst)))) Integer)
+      )
+      (input-filter inputs)
+   )
+)
+
+(define part1 : (Listof Integer) (combine-filtered lines))
+
+; Part 2
+
+(define replacements : (Listof (List String String))
+   (list
+      '("one" "o1e")
+      '("two" "t2o")
+      '("three" "t3e")
+      '("four" "4")
+      '("five" "5e")
+      '("six" "6")
+      '("seven" "7")
+      '("eight" "e8t")
+      '("nine" "9e")
+   )
+)
+
+(define replaced : (Listof String)
+   (map
+      (lambda ([line : String]): String
+         (foldl
+            (lambda ([val : (List String String)] [acc : String]) : String
+               (string-replace acc (first val) (last val))
+            )
+            line
+            replacements
+         )
+      )
+      lines
+   )
+)
+
+(define part2 : (Listof Integer) (combine-filtered replaced))
+
+(define day1 (list (foldr + 0 part1) (foldr + 0 part2)))
 
 
 (provide day1)
